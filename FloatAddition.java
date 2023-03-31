8f67295ab5586fbd33535f1978f2ee04391c23fd
claragoe <129517587+claragoe@users.noreply.github.com>
2023-03-31 14:51:23 +0200

    Add files via upload

---
+++ FloatAddition.java
@@ -0,0 +1,10 @@
+package aufgaben;
+
+public class FloatAddition {
+
+	public static void main(String[] args) {
+		float zahleins = 47.3f, zahlzwei = 2.3f;
+		float ergebnis = zahleins + zahlzwei;
+		System.out.println(ergebnis);
+	}
+} 
